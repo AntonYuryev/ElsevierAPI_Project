ffbbd4bfc9d30d0ce156de739bb3830255636e19
Local Sync <sync@local.dev>
2026-02-13 23:56:54 +0000

    drugs4disease butification

--- scripts/drugs4diseases.py
+++ scripts/drugs4diseases.py
@@ -5,8 +5,6 @@ from ElsevierAPI.api.ResnetAPI.PathwayStudioZeepAPI import load_api_config
 
 def do_the_job(dt:Drugs4Targets):
   dt.make_report()
-  # dt.add_etm_refs('Drugs',['glioma'])
-
   report_fpath = dt.report_path('.xlsx')
   dt.print_report(report_fpath)
 
@@ -20,21 +18,20 @@ if __name__ == "__main__":
     print(f'Script was started at {datetime.now()}')
     start = time.time()
     parameters_list = list(json.load(open("configs/drugs4diseases_parameters.json", "r")))
-    
+    job_count = 0
     for parameters in parameters_list:
       assert isinstance(parameters,dict)
-      skip = parameters.pop('skip', False)
-      if not skip:
+      if not parameters.pop('skip', False):
+        job_count += 1
+        dt = Drugs4Targets(load_api_config(), **parameters)
+        log_path = dt.report_path('.log')
         with Tee(log_path):
           disease_start = time.time()
           print(f'Finding drugs for {parameters["disease"]}')
-          print(f'Script started at {datetime.fromtimestamp(disease_start)}')
-          dt = Drugs4Targets(load_api_config(), **parameters)
-          log_path = dt.report_path('.log')
+          print(f'Job started at {datetime.fromtimestamp(disease_start)}')
           print(f'Runtime messages will be written to "{log_path}"')  
           do_the_job(dt)
-
-        print(f'{parameters["disease"]} finished in {execution_time(disease_start)} at {datetime.now()}')
-        print(f'Total script execution time: {execution_time(start)}')
-
+          print(f'{parameters["disease"]} finished in {execution_time(disease_start)} at {datetime.now()}')
+          print(f'Total job execution time: {execution_time(start)}')
+    print(f'Script ran with {job_count} input parameters')
     print(f'Script finished in {execution_time(start)} at {datetime.now()}')
